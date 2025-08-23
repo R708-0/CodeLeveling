4625996e2fa7778543126d80f5594efe71841ab5
Diego Melgar Parada <47795330+R708-0@users.noreply.github.com>
2025-08-23 21:09:48 +0000

    flask 1.2

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
+import sqlite3
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+def get_skills():
+    db = sqlite3.connect('leveling.db')
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM skills')
+    rows = cursor.fetchall()
+    db.close()
+
+    skills = []
+    for row in rows:
+        percent = int(row["xp"] / row["xp_max"]) * 100
+        skills.append(
+            {
+            'name': row["name"],
+            'level': row["level"],
+            'xp': row["xp"],
+            'xp_max': row["xp_max"],
+            'percent': percent,
+            'icon': row["icon"],
+            'color': row["color"]
+            }
+        )
+    return skills
 
 @app.route("/")
 def perfil():
@@ -13,8 +38,8 @@ def tareas():
 
 @app.route("/habilidades")
 def habilidades():
-    skills = db.execute
-    return render_template("habilidades.html")
+    skills = get_skills()
+    return render_template("habilidades.html", skills=skills)
 
 
 if __name__ == "__main__":
